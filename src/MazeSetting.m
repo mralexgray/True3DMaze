665a4c5cba5492a1c2bd1d081ec2888b90589653
Alex Gray <alex@mrgray.com>
2013-06-18 08:27:07 -0400

    Revert "Revert "Revert "Revert "fucked something up""""
    This reverts commit ed23f7af8324a99e5ad583630a5f8b8a713f4fcc.

--- src/MazeSetting.m
+++ src/MazeSetting.m
@@ -8,54 +8,26 @@
 
 #import "MazeSetting.h"
 
-@interface MazeSetting(Private)
-- (void)notifyAppearanceChanged;
-@end
-
 @implementation MazeSetting
 
-@synthesize mazeSizeX, mazeSizeY, mazeSizeZ, opaque, borderWidth, cornerRadius, wallColor, throughWalls, freeRotation;
+- (id)init	{	return self = [super init] ?
 
-static BOOL hasInitialized = NO;
-static NSArray *appearanceKeys;
-+ (void)initialize
-{
-    if (!hasInitialized) {
-//        MazeAppearanceSettingDidChangeNotification = @"MazeAppearanceSettingDidChangeNotification";
-//        appearanceKeys = ;
-        hasInitialized = YES;
-    }
+	_cellSize  		= 25.0f,
+	_mazeSizeX		= 5,
+	_mazeSizeY 		= 6,
+	_mazeSizeZ 		= 6,
+	_opaque 			= 0.7f,
+	_borderWidth 	= 0.5f,
+	_cornerRadius 	= 2.0f,
+	_wallColor 		= [NSColor whiteColor],
+	_throughWalls 	= NSOnState,
+	_freeRotation 	= NSOnState, self : nil;
 }
 
-- (id)init
-{
-    if (self = [super init]) {
-        self.mazeSizeX = 7;
-        self.mazeSizeY = 7;
-        self.mazeSizeZ = 7;
-        self.opaque = 0.7f;
-        self.borderWidth = 0.5f;
-        self.cornerRadius = 2.0f;
-        self.wallColor = [NSColor whiteColor];
-        self.throughWalls = NSOffState;
-        self.freeRotation = NSOffState;
-    }
-    return self;
+- (void)didChangeValueForKey:(NSString *)key	{
+	NSLog(@"%@  key: %@", NSStringFromSelector(_cmd), key);
+   [super didChangeValueForKey:key];
+	[@[@"cellSize", @"opaque", @"borderWidth", @"cornerRadius", @"wallColor"] containsObject:key] ?
+	[NSNotificationCenter postNotificationName:MazeAppearanceChanged object:self userInfo:@{key:[self valueForKey:key]}] : nil;
 }
-- (void)didChangeValueForKey:(NSString *)key
-{
-		NSLog(@"%@  key: %@", NSStringFromSelector(_cmd), key);
-    [super didChangeValueForKey:key];
-	if ([@[@"opaque", @"borderWidth", @"cornerRadius", @"wallColor"] containsObject:key])
-		[NSNotificationCenter postNotificationName:MazeAppearanceSettingDidChangeNotification object:self];
-}
-//        [self notifyAppearanceChanged];
-//    }
-//}
-//- (void)notifyAppearanceChanged
-//{
-//    [[NSNotificationCenter defaultCenter]
-//        postNotificationName:MazeAppearanceSettingDidChangeNotification
-//        object:self];
-//}
 @end
