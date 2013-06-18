b789a7789716da6dc6e763bf59ff74d30d026241
Alex Gray <alex@mrgray.com>
2013-06-18 08:27:07 -0400

    Revert "Revert "Revert "fucked something up"""
    This reverts commit b8f9de64288f5ada209dfecfc46c786e39be8966.

--- src/Maze3D.h
+++ src/Maze3D.h
@@ -8,9 +8,7 @@
 #import <QuartzCore/CoreAnimation.h>
 #import "MazeTypes.h"
 
-@class  MazeController;
-@interface Maze3D : NSObject
-{
+@interface Maze3D : NSObject {
     int sizeX;
     int sizeY;
     int sizeZ;
@@ -18,12 +16,10 @@
     MazePosition goal;
     char ***map;
 }
-@property (assign)  char ***map;
-@property (weak)	MazeController *controller;
 @property(readonly) int sizeX, sizeY, sizeZ;
 @property(readonly) MazePosition start, goal;
 
-- (void)constructMaze:(int)x :(int)y :(int)z controller:(MazeController*)c;
+- (void)constructMaze:(int)x :(int)y :(int)z;
 
 - (BOOL) isAtGoal: (MazePosition)pos;
 - (BOOL) hasWallAt:(MazePosition)pos              for:(WallDirection)direction;
@@ -31,8 +27,12 @@
 - (BOOL) position: (MazePosition)pos isAroundStartFor:(WallDirection)direction;
 @end
 
-@interface Maze3D ()
-- (unsigned)checkAround:(int)x :(int)y :(int)z :(CellType)value;
-- (void)allocateMap:(int)x :(int)y :(int)z;
-- (void)freeMap;
-@end
+#ifdef MAZE_3D_INCLUDE
+#define MAZE_3D_EXTERN
+#else
+#define MAZE_3D_EXTERN extern
+#endif
+
+
+// Notification names
+MAZE_3D_EXTERN NSString *Maze3DDidInitializeNotification;
